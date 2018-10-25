9b3c7ec30b1a47d257553e81ed20deaa3ebe44ad
Oscar Pinto <moka117@hotmail.com>
2018-10-25 17:46:02 -0500

    Avance HTML

--- src/main/java/com/parcial/Controller/WeatherController.java
+++ src/main/java/com/parcial/Controller/WeatherController.java
@@ -7,18 +7,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/Weather")
+@RestController()
+@RequestMapping(value = "/Weather")
 public class WeatherController {
 
     @Autowired
     WeatherService ws;
-
-    @RequestMapping()
-    public String getTestString(){
-
-
-        return "hola mundo!!";
-    }
 
     /* @PostMapping()
      public void postData(@RequestBody Todo todo){
--- src/main/resources/static/js/Back-EndWeather.js
+++ src/main/resources/static/js/Back-EndWeather.js
@@ -1,4 +1,14 @@
 var Back-EndModule =(function(){
+    return{
+    getWeather: async function(callback){
+                        orders = [];
+                            await Promise.resolve(axios.get('/orders'))
+                                    .then(async function(response){
+                                       orders = response.data;
+                                                    //PONER LA DATA
+
+                                                });
+    };
 
 
 })();
